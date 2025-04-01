ad75a801e4b47b6f36f3396ae944c9fcb09b4c0b
gabriela-carnevali <gabriela.c.lima10@aluno.senai.br>
2025-04-01 13:38:08 -0300

    Novo print
    Editei uma nova linha, pois commitei no lugar errado

--- principal.py
+++ principal.py
@@ -1,2 +1,3 @@
 print ("Estamos testando a criação de branch. Esse é o arquivo original")
 print ("Agora estou na branch raiz 1, e não no arquivo original")
+print ("Segund teste. Commitei no lugar errado")
